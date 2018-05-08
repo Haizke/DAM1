21ed237e6782bf2fb8724d9f375e7a33db2441bc
Haizke <Gabs@DESKTOP-UF4MQPS>
2018-05-09 01:37:52 +0200

    cambio a dados

--- Git/src/main/Clase4.java
+++ Git/src/main/Clase4.java
@@ -1,46 +1,57 @@
-public class SyGContarElementosRepetidos{
-    static int A=10;
-    static int B=20;
-    static int vectorA[]=new int[A];
-    static int vectorB[]=new int[B];
-    static int elemA=0; 
-    static int elemB=0;
-    static int z=0;
-    static void llenaArreglo(){
-        for(int i=0;i<vectorA.length;i++){
-            vectorA[i]=(int)(Math.random()*100+1);
-        }
- 
-        for(int i=0;i<vectorB.length;i++){
-            vectorB[i]=(int)(Math.random()*100+1);
-        }
- 
-    }
- 
-    static void compara(){
-        for(int i=0;i<vectorA.length;i++){
-            for(int j=0;j<vectorB.length;j++){
-                if(vectorA[i]==vectorB[j])
-                    elemA++;
-            }
-            System.out.println("El elemento "+vectorA[i]+" se presenta "+elemA+" veces en B");
-            elemA=0;
+package main;
+
+public class Clase4{
+    static int MAX=20;
+    static int a1[]=new int[MAX];
+    static int a2[]=new int[MAX];
+    static int a3[]=new int[MAX];
+    static int pares[]=new int[MAX];
+     
+    static void rellenaraleatorios(int x[], int y[], int z[]){
+        for(int i=0;i<x.length;i++)
+        {
+            x[i]=(int)(Math.random()*6+1);
+            y[i]=(int)(Math.random()*6+1);
+            z[i]=(int)(Math.random()*6+1);
         }
     }
- 
-    static void imprimir(){
-        for(int i=0;i<vectorA.length;i++){
-            System.out.print("A"+"["+i+"]= "+vectorA[i]+"\n");
+         
+    static void imprimir(int x[],String dado1,int y[],String dado2,int z[],String dado3){
+        for(int i=0;i<x.length;i++){
+             
+            System.out.print("Lanzamiento "+(i+1)+" : \t"); 
+            System.out.print(dado1+"["+i+"]="+x[i]+"\t");
+            System.out.print(dado2+"["+i+"]="+y[i]+"\t");
+            System.out.println(dado3+"["+i+"]="+z[i]+"\t");
+            revisarVector(x[i],y[i],z[i]);
         }
-        for(int i=0;i<vectorB.length;i++){
-            System.out.print("B"+"["+i+"]="+vectorB[i]+"\n");
-        }
-    }
- 
-    public static void main(String[] args){
-        llenaArreglo();
-        imprimir();
-        compara();
     }
      
+    static void revisarVector(int dado1,int dado2,int dado3){
+         
+         
+        //for(int i=0;i<m.length;i++){
+            if(dado1==5 && dado2==3 && dado3==6)
+            {
+            System.out.println("100 veces la apuesta.");                
+            }
+//          else if(dado1==6 && dado2==6 && dado3==6)
+ 
+        //}
+        //return pos;
+    }
+     
+    public static void main(String[] args){
+        rellenaraleatorios(a1,a2,a3);
+        int ultimo=0;
+        imprimir(a1,"Dado 1",a2,"Dado 2",a3,"Dado 3");
+//      rellenaraleatorios(a2);
+//      imprimir(a2,"Dado 2");
+//      rellenaraleatorios(a3);
+//      imprimir(a3,"Dado 3");
+        //ultimo=extraerpares(a1,pares,0);
+        //imprimir(pares,"pares");
+        //ultimo=extraerpares(a2,pares,ultimo);
+        //imprimir(pares,"pares");
+    }
 }
